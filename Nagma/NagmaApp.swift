de2719b36a28b27e794ab81f11a0e53bc20188ae
Shrirang-Zend <128277571+Shrirang-Zend@users.noreply.github.com>
2024-10-04 20:04:54 +0530

    Initial Commit

---
+++ Nagma/NagmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NagmaApp.swift
+//  Nagma
+//
+//  Created by Shrirang Zend on 04/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NagmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NagmaTests/NagmaTests.swift
@@ -0,0 +1,17 @@
+//
+//  NagmaTests.swift
+//  NagmaTests
+//
+//  Created by Shrirang Zend on 04/10/24.
+//
+
+import Testing
+@testable import Nagma
+
+struct NagmaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
